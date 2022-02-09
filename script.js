de4e3f5824a021263db2c7388aa9fe5edaf7d503
Ryan Locascio <63062893+RyanL2020@users.noreply.github.com>
2022-02-09 18:53:09 +0000

    merge two arrays with concat() and sort()

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
 let list1 = [5,3,4,6,7]
 let list2 = [9,5,7,8,6,4]
 
-
+// one way to merge two arrays of numbers
 var mergeTwoLists = function(list1, list2) {
+// concat function combines two arrays and puts them in a new array
   newArray = list1.concat(list2)
+// sort function sorts the elements in the arrray in acsending order
   newSortedArray = newArray.sort()
  console.log(newSortedArray)
 };
