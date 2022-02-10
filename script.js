af70b69bb7f224ada2e1af1cf2e3324b41392841
Ryan Locascio <63062893+RyanL2020@users.noreply.github.com>
2022-02-10 15:19:31 +0000

    solutions for remove element

--- script.js
+++ script.js
@@ -1,13 +1,34 @@
-let list1 = [5,3,4,6,7]
-let list2 = [9,5,7,8,6,4]
+let l1 = [1,2,3]
+let l2 = [4,5,6]
 
-// one way to merge two arrays of numbers
-var mergeTwoLists = function(list1, list2) {
-// concat function combines two arrays and puts them in a new array
-  newArray = list1.concat(list2)
-// sort function sorts the elements in the arrray in acsending order
-  newSortedArray = newArray.sort()
- console.log(newSortedArray)
+// // one way to merge two arrays of numbers
+// var mergeTwoLists = function(list1, list2) {
+// // concat function combines two arrays and puts them in a new array
+//   newArray = list1.concat(list2)
+// // sort function sorts the elements in the arrray in acsending order
+//   newSortedArray = newArray.sort()
+//  console.log(newSortedArray)
+// };
+ let nums = [3,2,2,3]
+ let val = 3
+// var removeElement = function(nums, val) {
+//     while (nums.length < val ) {
+//       return nums
+//     }
+// };
+// var removeElement = function (nums, val) {
+//     let j = 0;
+//     for (let i = 0; i < nums.length; i++) {
+//         if (nums[i] !== val) {
+//             nums[j++] = nums[i];
+//         }
+//     }
+
+//     //nums.length = j; // Chop the surplus values - not needed for LC submission
+//     return j;
+// };
+var removeElement = function (nums, val) {
+    nums = nums.filter((num) => num !== val);
+    return nums;
 };
-
-console.log(mergeTwoLists(list1,list2))
+ console.log(removeElement(nums, val));
